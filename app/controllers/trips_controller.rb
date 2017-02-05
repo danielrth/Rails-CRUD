26de24849481687639998dba19ba7010faf4f1fe
danielrth <danielrth9@hotmail.com>
2017-02-05 23:01:26 +0900

    implement CRUD for trip

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,48 @@
+class TripsController < ApplicationController
+	def index
+		@trips = Trip.all
+	end
+	def new
+		@trip = Trip.new
+	end
+
+	def show
+		@trip = Trip.find(params[:id])
+	end
+
+	def edit
+	    @trip = Trip.find(params[:id])
+	end
+
+	def create
+	  @trip = Trip.new(params.require(:trip).permit(:title, :address))
+	 
+	  if @trip.save
+	  	redirect_to @trip
+	  else
+	  	render 'new'
+	  end
+	end
+
+	def update
+		@trip = Trip.find(params[:id])
+
+		if @trip.update(trip_params)
+		  redirect_to @trip
+		else
+		  render 'edit'
+		end
+	end
+
+	def destroy
+	  @trip = Trip.find(params[:id])
+	  @trip.destroy
+	 
+	  redirect_to trips_path
+	end
+
+	private
+		def trip_params
+			params.require(:trip).permit(:title, :address)
+		end
+end
---
+++ app/models/trip.rb
@@ -0,0 +1,3 @@
+class Trip < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 5 }
+end
